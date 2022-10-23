e4ff376c02cae2020509780943dc7b2eba325cb8
Jose Juan Cortina <juan.cortina.gpe@gmail.com>
2022-10-23 03:41:01 -0500

    Se agregan clases boton, id a los botones y hover a los botones

---
+++ Script/index.js
@@ -0,0 +1,23 @@
+// let elemento;
+// elemento = document;
+// elemento = document.title
+// elemento = document.head
+// console.log(elemento);
+//----
+// let elemento;
+// elemento = window
+// console.log(elemento);
+//----
+//let elemento = document.querySelector('calculadora');
+//let elemento = document.styleSheets;
+//let elemento = document.links
+//console.log(elemento);
+//----
+// const padre = document.querySelector('body')
+// console.log(padre.children)
+// const button = document.querySelector("decimal")
+// button.onclick=()=>{
+//     console.log("click funcion flecha")
+// }
+let elemento = document.getElementsByTagName("button")
+console.log(elemento);
