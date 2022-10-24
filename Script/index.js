e3110cf826703e99eb98fd0fd22ab4936b4996df
Jose Juan Cortina <juan.cortina.gpe@gmail.com>
2022-10-24 14:31:11 -0500

    Se asignan los botones con querrySelector a las constantes botones, error al intentar implementar funciones a botones

--- Script/index.js
+++ Script/index.js
@@ -19,5 +19,33 @@
 // button.onclick=()=>{
 //     console.log("click funcion flecha")
 // }
-let elemento = document.getElementsByTagName("button")
-console.log(elemento);
+// let elemento = document.getElementsByTagName("button")
+// console.log(elemento);
+
+const suma = document.querySelector("#btnSuma")
+const resta = document.querySelector("#btnResta")
+const mult = document.querySelector("#btnMultiplicacion")
+const div = document.querySelector("#btnDivision")
+const potencia = document.querySelector("#btnPotencia")
+const raiz = document.querySelector("#btnRaiz")
+const residuo = document.querySelector("#btnResiduo")
+const clear = document.querySelector("#btnClear")
+const mem1 = document.querySelector("#btnMemoria1")
+const mem2 = document.querySelector("#btnMemoria2")
+
+const btn0 = document.querySelector("#btn0")
+const btnDecimal = document.querySelector("#btnDecimal")
+const btnCambSig = document.querySelector("#btnCambioSigno")
+const btn1 = document.querySelector("#btn1")
+const btn2 = document.querySelector("#btn2")
+const btn3 = document.querySelector("#btn3")
+const btn4 = document.querySelector("#btn4")
+const btn5 = document.querySelector("#btn5")
+const btn6 = document.querySelector("#btn6")
+const btn7 = document.querySelector("#btn7")
+const btn8 = document.querySelector("#btn8")
+const btn9 = document.querySelector("#btn9")
+
+btn0.onclick=()=>{
+        console.log("Boton cero")
+    }
